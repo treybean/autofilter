b7c091632fb3d8bf84cc0c699fd33d1906ba50c3
Trey Bean <trey@12spokes.com>
2008-03-16 17:31:50 -0600

    wrote initialize function for Autofilter object

---
+++ src/autofilter.js
@@ -0,0 +1,9 @@
+var Autofilter = Class.create({
+  initialize: function(element, option_container) {
+    this.element = $(element);
+    this.option_container = $(option_container);
+    this.option_elements = this.option_container.childElements();
+  },
+  
+  
+});
