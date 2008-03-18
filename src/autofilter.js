addcc3c72c8be9998f641dbe51fe1b82ddf62491
Trey Bean <trey@12spokes.com>
2008-03-18 08:53:18 -0600

    finished work to autofilter first level of children based on innerHTML

--- src/autofilter.js
+++ src/autofilter.js
@@ -3,7 +3,16 @@ var Autofilter = Class.create({
     this.element = $(element);
     this.option_container = $(option_container);
     this.option_elements = this.option_container.childElements();
+    
+    Event.observe(this.element, 'keypress', this.filter_options.bindAsEventListener(this));
   },
   
-  
+  filter_options: function() {
+    split_options = this.option_elements.partition(function(n){
+      return n.innerHTML.match(new RegExp($F('af_input'), "i"));// == null ? false : true;
+    });
+    
+    split_options[1].invoke('hide');
+    split_options[0].invoke('show');
+  }
 });
